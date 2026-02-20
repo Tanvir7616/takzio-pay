8a37eff21e232e558eed117f0655b13f34f3c244
Tanvir7616 <tanvirahmedantu00@gmail.com>
2026-02-20 13:49:34 -0500

    first commit

---
+++ scripts/login.js
@@ -0,0 +1,30 @@
+console.log('connected')
+
+document.getElementById('login-btn').addEventListener("click", function(){
+    // input number 
+    const inputNumber = document.getElementById('input-number');
+    const number = inputNumber.value;
+    console.log(number);
+
+    // input pin 
+
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value;
+    console.log(pin);
+
+    // match number and pin 
+
+    if(number === "01991051850" && pin === "1234"){
+        // alert logged in true 
+
+        alert('Login Success');
+
+        window.location.assign("/home.html");
+
+    }else{
+        // alert log in failed false 
+
+        alert('Login Failed');
+        return;
+    }
+})
